cab9887296e47567f515468f5a838718b9b4ffc4
Zhanghao <yukiniro@hotmail.com>
2022-03-20 22:25:08 +0800

    docs: update readme and website

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # tinydash
 
 The `tinydash` A small JavaScript utility library.
+
+## Basic Use
+
+npm
+
+```shell
+npm i tinydash
+```
+
+pnpm
+
+```shell
+pnpm add tinydash
+```
+
+## Example
+
+```javascript
+import { isNaN } from 'tinydash'
+
+console.log(isNaN(1)); // false
+console.log(isNaN(NaN)); // true
+```
+
+## TODO
+
+- [x] `isNumber`
+- [x] `isUndefined`
+- [x] `isString`
+- [ ] `isObject`
+- [ ] `isArray`
+- [ ] `isNull`
+- [x] `isNaN`
+- [ ] `isBoolean`
+- [ ] `isFalsy`
+- [ ] `isTruthy`
+- [x] `sleep`
+- [ ] `polling`
+- [ ] `chunk`
+- [ ] `clone`
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Tinydash',
+  tagline: 'A small JavaScript utility library.',
+  url: 'https://github.com/Yukiniro/tinydash',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Yukiniro', // Usually your GitHub org/user name.
+  projectName: 'tinydash', // Usually your repo name.
 
   presets: [
     [
@@ -35,7 +35,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Tinydash',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -48,7 +48,7 @@ const config = {
             label: 'Tutorial',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/Yukiniro/tinydash',
             label: 'GitHub',
             position: 'right',
           },
@@ -58,43 +58,12 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
+            label: 'Tutorial',
+            to: '/docs/intro',
           },
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
+            label: 'GitHub',
+            href: 'https://github.com/facebook/docusaurus',
           },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
