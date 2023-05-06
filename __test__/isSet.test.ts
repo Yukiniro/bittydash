75cc8f86ba499b248be982cd72b34cfd637137e0
Yukiniro <yukiniro@hotmail.com>
2023-05-06 23:38:26 +0800

    chore: update isSet test

--- __test__/isSet.test.ts
+++ __test__/isSet.test.ts
@@ -1,22 +1,12 @@
 import { test, expect } from "vitest";
 import { isSet } from "../src";
+import { getTestValues } from "./util";
 
-test("isSet", () => {
-  expect(isSet(0)).toBe(false);
-  expect(isSet(1)).toBe(false);
-  expect(isSet(-1)).toBe(false);
-  expect(isSet(Infinity)).toBe(false);
-  expect(isSet(-Infinity)).toBe(false);
-  expect(isSet(NaN)).toBe(false);
-  expect(isSet("1")).toBe(false);
-  expect(isSet(undefined)).toBe(false);
-  expect(isSet(null)).toBe(false);
-  expect(isSet([])).toBe(false);
-  expect(isSet({})).toBe(false);
-  expect(isSet(true)).toBe(false);
-  expect(isSet(false)).toBe(false);
-  expect(isSet(() => {})).toBe(false);
-  expect(isSet(new Set())).toBe(true);
-  expect(isSet(new Map())).toBe(false);
-  expect(isSet(new ArrayBuffer(1))).toBe(false);
+const values = getTestValues().map((value, index) => ({
+  index,
+  expected: isSet(value),
+}));
+
+test.each(values)("isSet($index) -> $expected", ({ index, expected }) => {
+  expect([14].includes(index)).toBe(expected);
 });
